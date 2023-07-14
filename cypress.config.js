26d1fe8bbfd07b64ac05aed7e4025f6c41f3463a
User <user@Users-MacBook-Pro.local>
2023-07-14 14:29:17 +0800

    Add reported

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://uat-eservices.fwd.com.hk/',
     defaultCommandTimeout: 60000,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
